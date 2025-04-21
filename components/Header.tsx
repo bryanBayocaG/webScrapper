d85e08989218dd4364d5f20fd7238bfe1c15d90b
Bryan Adanza Bayoca <123434159+bryanBayocaG@users.noreply.github.com>
2025-04-21 22:57:16 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ import {
   SelectItem,
 } from "@tremor/react";
 import SearchButton from './SearchButton';
-import Avatar from 'react-avatar';
+import Avatar from 'react-avata r';
 import { useState } from "react";
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation'
